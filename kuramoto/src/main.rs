32fa06c7faf1c69f7bcd07557508921f9769f1bf
jkthms <jakeleediffusion@gmail.com>
2025-10-18 10:44:59 +0100

    test: Check config loader works correctly

--- kuramoto/src/main.rs
+++ kuramoto/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+use config::Config;
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let config = Config::from_file("config.toml")?;
+    println!("Loaded configuration: {:?}", config);
+    Ok(())
 }
